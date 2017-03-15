71cfa60d22cf52b8d4789ac826368ede22dae6de
nachoparker <nacho@ownyourbits.com>
2017-03-15 08:05:10 +0100

    batch build improvements

--- installer.sh
+++ installer.sh
@@ -35,14 +35,14 @@ IP=$2                   # Second argument is the QEMU Raspbian IP address
 IMGFILE=$3              # Third argument is the image file to start from ( empty for online installation )
 CONFDIR=/usr/local/etc/nextcloudpi-config.d/
  
-source library.sh       # initializes $IMGOUT
+source library.sh       # initializes $IMGNAME
 
 config $INSTALL_SCRIPT || exit 1
 
 if [[ "$IMGFILE" != "" ]]; then
-  launch_install_qemu "$IMGFILE" $IP || exit 1
-  copy_to_image $( ls -1t $IMGFILE-stage* | head -1 ) $CONFDIR $INSTALL_SCRIPT || exit 1
-  pack_image          "$IMGFILE" "$IMGOUT" 
+  launch_install_qemu "$IMGFILE" $IP                      || exit 1    # initializes $IMGOUT
+  copy_to_image       "$IMGOUT"  $CONFDIR $INSTALL_SCRIPT || exit 1
+  pack_image          "$IMGOUT" "$IMGNAME" 
 else
   launch_installation_online $IP
 fi
