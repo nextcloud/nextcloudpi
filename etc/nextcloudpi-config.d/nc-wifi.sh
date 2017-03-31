d7ced8da65b9f0c57e8c9b0f421df2c2422daad8
nachoparker <nacho@ownyourbits.com>
2017-03-31 17:43:25 +0200

    added wifi-curses

---
+++ etc/nextcloudpi-config.d/nc-wifi.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Data dir configuration script for NextCloudPi
+# Tested with 2017-03-02-raspbian-jessie-lite.img
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Usage:
+# 
+#   ./installer.sh nc-wifi.sh <IP> (<img>)
+#
+# See installer.sh instructions for details
+#
+# More at https://ownyourbits.com/
+#
+
+DESCRIPTION="Configure your Wi-Fi connection"
+
+install()
+{
+  apt-get update
+  apt install -y --no-install-recommends wicd-curses
+}
+
+configure()
+{
+  whiptail --msgbox \
+           --backtitle "NextCloudPi configuration" \
+           --title "Instructions to configure Wi-Fi" \
+"1) Select a Wi-Fi network
+2) Press right arrow ->
+3) Enter the passphrase for your Wi-Fi
+4) Make sure to select 'connect automatically'
+5) F10 to save
+6) C to connect" \
+  20 90
+  wicd-curses
+}
+
+cleanup()
+{
+  apt-get autoremove -y
+  apt-get clean
+  rm /var/lib/apt/lists/* -r
+  rm -f /home/pi/.bash_history
+  systemctl disable ssh
+}
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
