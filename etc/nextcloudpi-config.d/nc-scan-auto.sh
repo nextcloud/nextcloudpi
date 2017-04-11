ac7e397ab875e74a7d6a824bb1f11d5c3a9ddd38
nacho <nacho.nunez@aoifes.com>
2017-04-11 21:49:18 +0200

    NFS and nc-scan

---
+++ etc/nextcloudpi-config.d/nc-scan-auto.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Periodically synchronize NextCloud for externally modified files
+# Tested with 2017-03-02-raspbian-jessie-lite.img
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Usage:
+# 
+#   ./nc-scan-auto.sh
+#
+# See installer.sh instructions for details
+# More at: https://ownyourbits.com
+#
+
+SCANINTERVAL_=60
+DESCRIPTION="Periodically scan NC for externally modified files"
+
+configure() 
+{
+  whiptail --msgbox \
+    --backtitle "NextCloudPi configuration" \
+    --title "Instructions for auto synchronization" \
+"Set the time in minutes in SCANINTERVAL. \n
+>>> If there are too many files this can greatly affect performance. <<< \n
+SCANINTERVAL=0 will disable periodic synchronization." \
+      20 90
+
+    [[ $SCANINTERVAL_ == 0 ]] && { 
+    systemctl stop    nc-scan.timer
+    systemctl disable nc-scan.timer
+    return 0
+  }
+
+    cat > /etc/systemd/system/nc-scan.timer <<EOF
+    [Unit]
+    Description=Timer to scan NC for externally modified files
+
+    [Timer]
+    OnBootSec=${SCANINTERVAL_}min
+    OnUnitActiveSec=${SCANINTERVAL_}min
+    Unit=nc-scan.service
+
+    [Install]
+    WantedBy=timers.target
+EOF
+  cat > /etc/systemd/system/nc-scan.service <<EOF
+[Unit]
+Description=Scan NC for externally modified files
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/ncp-scan
+
+[Install]
+WantedBy=default.target
+EOF
+  systemctl daemon-reload
+  systemctl enable nc-scan.timer
+  systemctl start  nc-scan.timer
+}
+
+install() { :; }
+cleanup() { :; }
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
---
+++ etc/nextcloudpi-config.d/nc-scan.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Synchronize NextCloud for externally modified files
+# Tested with 2017-03-02-raspbian-jessie-lite.img
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Usage:
+# 
+#   ./nc-scan.sh
+#
+# See installer.sh instructions for details
+# More at: https://ownyourbits.com
+#
+
+DESCRIPTION="Scan NC for externally modified files"
+
+install() 
+{ 
+  cat > /usr/local/bin/ncp-scan <<EOF
+cd /var/www/nextcloud
+sudo -u www-data php occ files:scan --all
+EOF
+  chmod +x /usr/local/bin/ncp-scan
+}
+
+configure() 
+{
+  /usr/local/bin/ncp-scan
+}
+
+cleanup() { :; }
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
