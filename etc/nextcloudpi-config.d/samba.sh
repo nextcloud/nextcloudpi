8589e1efd71e037357c5d1deab719484f4d276f3
nacho <nacho.nunez@aoifes.com>
2017-04-14 15:26:18 +0200

    added samba/cifs

--- etc/nextcloudpi-config.d/samba.sh
+++ etc/nextcloudpi-config.d/samba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NFS server for Raspbian 
+# SAMBA/CIFS server for Raspbian 
 # Tested with 2017-03-02-raspbian-jessie-lite.img
 #
 # Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
@@ -8,23 +8,27 @@
 #
 # Usage:
 # 
-#   ./installer.sh NFS.sh <IP> (<img>)
+#   ./installer.sh samba.sh <IP> (<img>)
 #
 # See installer.sh instructions for details
 # More at: https://ownyourbits.com
 #
 
 DIR_=/media/USBdrive/ncdata/admin/files
-SUBNET_=192.168.1.0/24
-USER_=www-data
-GROUP_=www-data
-DESCRIPTION="NFS network file system server (for Linux LAN)"
+USER_=pi
+PWD_=raspberry
+DESCRIPTION="SAMBA/CIFS file server (for Mac/Linux/Windows)"
+
+# mkdir -p /var/log/samba in init.d
 
 install()
 {
   apt-get update
-  apt-get install --no-install-recommends -y nfs-kernel-server 
-  systemctl disable nfs-kernel-server
+  apt-get install --no-install-recommends -y samba
+  update-rc.d smbd disable
+
+  # the directory needs to be recreated if we are using nc-ramlogs
+  grep -q mkdir /etc/init.d/smbd || sed -i "/\<start)/amkdir -p /var/log/samba" /etc/init.d/smbd
 }
 
 configure()
@@ -34,7 +38,7 @@ configure()
   whiptail --msgbox \
            --backtitle "NextCloudPi configuration" \
            --title "Instructions for external synchronization" \
-"If we intend to modify the data folder through NFS,
+"If we intend to modify the data folder through SAMBA,
 then we have to synchronize NextCloud to make it aware of the changes. \n
 This can be done manually or automatically using 'nc-scan' and 'nc-scan-auto' 
 from 'nextcloudpi-config'" \
@@ -48,47 +52,22 @@ from 'nextcloudpi-config'" \
 
   # CONFIG
   ################################
-  cat > /etc/exports <<EOF
-$DIR_ $SUBNET_(rw,sync,all_squash,anonuid=$(id -u $USER_),anongid=$(id -g $GROUP_),no_subtree_check)
+  sed -i '/\[NextCloudPi\]/,+5d' /etc/samba/smb.conf
+  cat >> /etc/samba/smb.conf <<EOF
+[NextCloudPi]
+	path = $DIR_
+	writeable = yes
+;	browseable = yes
+	valid users = $USER_
 EOF
 
-  cat > /etc/systemd/system/nfs-common.services <<EOF
-[Unit]
-Description=NFS Common daemons
-Wants=remote-fs-pre.target
-DefaultDependencies=no
+  update-rc.d smbd defaults
+  update-rc.d smbd enable
+  service smbd start
 
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/etc/init.d/nfs-common start
-ExecStop=/etc/init.d/nfs-common stop
-
-[Install]
-WantedBy=sysinit.target
-EOF
-
-  cat > /etc/systemd/system/rpcbind.service <<EOF
-[Unit]
-Description=RPC bind portmap service
-After=systemd-tmpfiles-setup.service
-Wants=remote-fs-pre.target
-Before=remote-fs-pre.target
-DefaultDependencies=no
-
-[Service]
-ExecStart=/sbin/rpcbind -f -w
-KillMode=process
-Restart=on-failure
-
-[Install]
-WantedBy=sysinit.target
-Alias=portmap
-EOF
-
-  systemctl enable rpcbind
-  systemctl enable nfs-kernel-server
-  service nfs-kernel-server restart
+  usermod -aG www-data $USER_
+  echo -e "$PWD_\n$PWD_" | smbpasswd -s -a $USER_
+  sudo chmod g+w $DIR_
 }
 
 cleanup()
@@ -116,3 +95,4 @@ cleanup()
 # along with this script; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330,
 # Boston, MA  02111-1307  USA
+
