10488be24bdfb3940c02c53bfbe26334daead928
nachoparker <nacho@ownyourbits.com>
2017-11-10 17:27:37 +0100

    update: wait running apt processes (fix)

--- build-devel.sh
+++ build-devel.sh
@@ -17,7 +17,14 @@
 # this is a replica of ncp-update, but from devel branch
 install() 
 {
-  test -f /etc/apt/apt.conf.d/20nextcloudpi-upgrades && { echo "deactivate unattended upgrades first"; return 1; }
+  # wait for other apt processes
+  pgrep apt &>/dev/null && echo "waiting for apt processes to finish..." && \
+  while :; do
+    pgrep apt &>/dev/null || break
+    sleep 1
+  done
+  rm -f /etc/apt/apt.conf.d/20nextcloudpi-upgrades
+
   echo -e "Downloading updates"
   rm -rf /tmp/ncp-update-tmp
   git clone --depth 20 -q -b devel https://github.com/nextcloud/nextcloudpi.git /tmp/ncp-update-tmp
