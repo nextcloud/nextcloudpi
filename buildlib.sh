b26d2b79e1253c9c402b00e4110b8461b5cf7bf2
nacho <nacho.nunez@aoifes.com>
2017-11-18 14:05:42 +0100

    build: add prepare_sshd

--- buildlib.sh
+++ buildlib.sh
@@ -223,3 +223,15 @@ function deactivate_unattended_upgrades()
   sudo umount -l tmpmnt
   rmdir tmpmnt &>/dev/null
 }
+
+function prepare_sshd()
+{
+  local IMG="$1"
+  local SECTOR1=$( fdisk -l $IMG | grep FAT32 | awk '{ print $2 }' )
+  local OFFSET1=$(( SECTOR1 * 512 ))
+  mkdir -p tmpmnt
+  sudo mount $IMG -o offset=$OFFSET1 tmpmnt
+  sudo touch tmpmnt/ssh   # this enables ssh
+  sudo umount tmpmnt
+  rmdir tmpmnt
+}
