1563373fbe5a5b0a59998ef5c7453310836173f2
nacho <nacho.nunez@aoifes.com>
2017-11-19 11:43:31 +0100

    added nc-info

---
+++ etc/nextcloudpi-config.d/nc-info.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Print NCP sytem info
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Usage:
+# 
+#   ./installer.sh nc-diag.sh <IP> (<img>)
+#
+# See installer.sh instructions for details
+# More at: https://ownyourbits.com
+#
+
+DESCRIPTION="Print NextCloudPi system info"
+
+configure() 
+{
+  ncp-diag | column  -t -s'|'
+}
+
+install() { :; }
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
