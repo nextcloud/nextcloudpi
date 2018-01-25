3f6fa6e5f0c6fa82fb727fa23607398581664226
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2018-01-25 02:24:43 +0100

    Change variable naming to comply with conventions.

--- ncp-web/L10N.php
+++ ncp-web/L10N.php
@@ -2,33 +2,33 @@
 
 class L10N {
     private $translations = [];
-    const defaultLanguage = "en_US";
+    const default_language = "en_US";
     private $language;
 
     /**
      * @throws Exception
      **/
-    public function __construct($desiredLanguages, $l10nDir)
+    public function __construct($desired_languages, $l10n_dir)
     {
-        if(!isset($desiredLanguages)) {
-            $desiredLanguages = "";
+        if (!isset($desired_languages)) {
+            $desired_languages = "";
         }
 
-        $l10nDir = trim($l10nDir, '/');
-        $availableLanguages = array_filter(scandir($l10nDir),
-            function($s) {
+        $l10n_dir = trim($l10n_dir, '/');
+        $available_languages = array_filter(scandir($l10n_dir),
+            function ($s) {
                 return pathinfo($s, PATHINFO_EXTENSION) == "json";
             });
-        $availableLanguages = array_map(
-                function($s) {
-                    return basename($s, ".json");
-                },
-                $availableLanguages);
-        $lang = $this->find_language($availableLanguages, $desiredLanguages);
-        $full_path = join('/', [$l10nDir, $lang.".json"]);
+        $available_languages = array_map(
+            function ($s) {
+                return basename($s, ".json");
+            },
+            $available_languages);
+        $lang = $this->find_language($available_languages, $desired_languages);
+        $full_path = join('/', [$l10n_dir, $lang . ".json"]);
         $this->language = $lang;
-        if( !file_exists($full_path) || !$this->load($full_path) ) {
-            $this->language = L10N::defaultLanguage;
+        if ($lang === L10N::default_language || !file_exists($full_path) || !$this->load($full_path)) {
+            $this->language = L10N::default_language;
         }
     }
 
@@ -50,23 +50,23 @@ class L10N {
         return $text;
     }
 
-    function find_language(array $availableLanguages, $desiredLanguage) {
+    function find_language(array $available_languages, $desired_language) {
 
-        $availableLanguages = array_flip($availableLanguages);
+        $available_languages = array_flip($available_languages);
 
         $langs = [];
-        preg_match_all('~([\w-]+)(?:[^,\d]+([\d.]+))?~', strtolower($desiredLanguage), $matches, PREG_SET_ORDER);
+        preg_match_all('~([\w-]+)(?:[^,\d]+([\d.]+))?~', strtolower($desired_language), $matches, PREG_SET_ORDER);
         foreach($matches as $match) {
 
             list($a, $b) = explode('-', $match[1]) + array('', '');
             $value = isset($match[2]) ? (float) $match[2] : 1.0;
 
-            if(isset($availableLanguages[$match[1]])) {
+            if(isset($available_languages[$match[1]])) {
                 $langs[$match[1]] = $value;
                 continue;
             }
 
-            if(isset($availableLanguages[$a])) {
+            if(isset($available_languages[$a])) {
                 $langs[$a] = $value - 0.1;
             }
 
@@ -74,7 +74,7 @@ class L10N {
         arsort($langs);
 
         if(count($langs) == 0) {
-            return L10N::defaultLanguage;
+            return L10N::default_language;
         }
         return array_keys($langs)[0];
     }
