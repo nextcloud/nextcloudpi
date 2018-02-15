ed3f306c23b84f297af213ba7afa116042aa1f18
Aeon512 <aeon512@gmail.com>
2018-02-15 21:54:34 +0100

    Reset Redis Password if necessary
    During bootup of a new docker image, the redis
    password might not match the nextcloud configuration.
    Hence, we automatically update the nextcloud configuration.
    
    For the MariaDB password the same method is applied.
    
    Additionaly identical files have been moved to docker-common
    to simplify changes in the future

---
+++ docker-common/nextcloud/ncp-provisioning.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REDISPASS="$( grep "^requirepass" /etc/redis/redis.conf | cut -f2 -d' ' )"
+# IF redis password is the default one, generate a new one
+[[ "$REDISPASS" == "default" ]] && {
+  REDISPASS="$( openssl rand -base64 32 )"
+  echo Provisioning Redis password with $REDISPASS
+  sed -i -E "s|^requirepass .*|requirepass $REDISPASS|" /etc/redis/redis.conf
+}
+# If there exists already a configuration adjust the password
+test -f /data/app/config/config.php && {
+  echo Updating NextCloud config with Redis password $REDISPASS
+  sed -i "s|'password'.*|'password' => '$REDISPASS',|" /data/app/config/config.php
+}
+
+DBADMIN=ncadmin
+DBPASSWD=$( grep password /root/.my.cnf | cut -d= -f2 )
+[[ "$DBPASSWD" == "default" ]] && {
+  DBPASSWD=$( openssl rand -base64 32 )
+  echo Provisioning MariaDB password with $DBPASSWD
+  echo -e "[client]\npassword=$DBPASSWD" > /root/.my.cnf
+  chmod 600 /root/.my.cnf
+  mysql <<EOF
+GRANT USAGE ON *.* TO '$DBADMIN'@'localhost' IDENTIFIED BY '$DBPASSWD';
+DROP USER '$DBADMIN'@'localhost';
+CREATE USER '$DBADMIN'@'localhost' IDENTIFIED BY '$DBPASSWD';
+GRANT ALL PRIVILEGES ON nextcloud.* TO $DBADMIN@localhost;
+EXIT
+EOF
+}
+test -f /data/app/config/config.php && {
+  echo Updating NextCloud config with MariaDB password $DBPASSWD
+  sed -i "s|'dbpassword' =>.*|'dbpassword' => '$DBPASSWD',|" /data/app/config/config.php
+}
