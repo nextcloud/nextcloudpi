1830d774d17ff25b3230ac3932fc779b857bfb86
nachoparker <nacho@ownyourbits.com>
2018-03-18 21:12:23 +0100

    nc-ramlogs: fix enabled by default upon installoation

--- etc/nextcloudpi-config.d/nc-ramlogs.sh
+++ etc/nextcloudpi-config.d/nc-ramlogs.sh
@@ -25,6 +25,7 @@ install()
   curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
   tar xf log2ram.tar.gz
   cd log2ram-master
+  sed -i '/systemctl enable log2ram/d' install.sh
   chmod +x install.sh && sudo ./install.sh
   cd ..
   rm -r log2ram-master log2ram.tar.gz
