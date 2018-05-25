a629e633ebcd9cb58fc3d76d348296b8db38e25b
Ignacio Nunez <nacho@ownyourbits.com>
2018-05-25 13:32:42 +0200

    nc-ramlogs: fix docker installation from latest upstream changes

--- etc/ncp-config.d/nc-ramlogs.sh
+++ etc/ncp-config.d/nc-ramlogs.sh
@@ -26,6 +26,7 @@ install()
   curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
   tar xf log2ram.tar.gz
   cd log2ram-master
+  sed -i '/systemctl -q is-active log2ram/d' install.sh
   sed -i '/systemctl enable log2ram/d' install.sh
   chmod +x install.sh && sudo ./install.sh
   cd ..
