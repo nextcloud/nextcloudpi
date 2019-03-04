41601e94edb61596fea35017c28296d5b738e4d2
nachoparker <nacho@ownyourbits.com>
2019-03-03 19:33:25 -0700

    cleanup update.sh

--- update.sh
+++ update.sh
@@ -141,40 +141,6 @@ cp -r ncp-app /var/www/
   # update to the latest version
   is_active_app nc-autoupdate-nc && run_app nc-autoupdate-nc
 
-  # fix Armbian cron bug
-  [[ "$( ls -1 /etc/cron.d/ |  wc -l )" -gt 0 ]] && chmod 644 /etc/cron.d/*
-  [[ "$( ls -1 /etc/cron.daily/ |  wc -l )" -gt 0 ]] && chmod 755 /etc/cron.daily/*
-  [[ "$( ls -1 /etc/cron.hourly/ |  wc -l )" -gt 0 ]] && chmod 755 /etc/cron.hourly/*
-
-  # change letsencrypt from package based to git based
-  [[ -f /etc/letsencrypt/certbot-auto ]] || {
-    echo "updating letsencrypt..."
-    [[ -f /.docker-image ]] && mv "$(readlink /etc/letsencrypt)" /etc/letsencrypt-old
-    [[ -f /.docker-image ]] || mv /etc/letsencrypt /etc/letsencrypt-old
-    rm -f /etc/letsencrypt
-    apt-get remove -y letsencrypt
-    apt-get autoremove -y
-    install_app letsencrypt || { rm -rf /etc/letsencrypt; mv /etc/letsencrypt-old /etc/letsencrypt; exit 1; }
-    [[ -f /etc/letsencrypt-old/live ]] && cp -raT /etc/letsencrypt-old/live /etc/letsencrypt/live
-    [[ -f /.docker-image ]] && persistent_cfg /etc/letsencrypt
-    [[ -f /etc/cron.weekly/letsencrypt-ncp ]] && run_app letsencrypt
-  }
-
-  # fix LE update bug
-  [[ -d /etc/letsencrypt/archive ]] || {
-    sleep 3
-    cp -ravT /etc/letsencrypt-old/archive /etc/letsencrypt/archive &>/dev/null || true
-    bash -c "sleep 2 && service apache2 reload" &>/dev/null &
-  }
-
-  # fix LE update bug (2)
-  if [[ -d /etc/letsencrypt/archive ]] && [[ "$(ls /etc/letsencrypt/archive/* 2>/dev/null | wc -l )" == "0" ]]; then
-    rmdir /etc/letsencrypt/archive
-    sleep 3
-    cp -ravT /etc/letsencrypt-old/archive /etc/letsencrypt/archive &>/dev/null || true
-    bash -c "sleep 2 && service apache2 reload" &>/dev/null &
-  fi
-
   # configure MariaDB (UTF8 4 byte support)
   [[ -f /etc/mysql/mariadb.conf.d/91-ncp.cnf ]] || {
     cat > /etc/mysql/mariadb.conf.d/91-ncp.cnf <<EOF
