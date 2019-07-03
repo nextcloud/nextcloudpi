521c03ff959b1cedfaf111301c1873089780a97e
nachoparker <nacho@ownyourbits.com>
2019-07-03 16:17:00 -0600

    build: dont include imagick
    Signed-off-by: nachoparker <nacho@ownyourbits.com>

--- bin/ncp/CONFIG/nc-nextcloud.sh
+++ bin/ncp/CONFIG/nc-nextcloud.sh
@@ -25,7 +25,8 @@ install()
   apt-get update
   $APTINSTALL lbzip2 iputils-ping jq
   $APTINSTALL -t $RELEASE php-smbclient exfat-fuse exfat-utils                  # for external storage
-  $APTINSTALL -t $RELEASE imagemagick php${PHPVER}-imagick php${PHPVER}-exif    # for gallery
+  $APTINSTALL -t $RELEASE php${PHPVER}-exif                                     # for gallery
+  #$APTINSTALL -t imagemagick php${PHPVER}-imagick   # for gallery
 
 
   # POSTFIX
