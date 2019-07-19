ed6d53b261fd1a0cbf6ef648a994f2f5df4c564b
nachoparker <nacho@ownyourbits.com>
2019-07-19 16:52:33 -0600

    raspi: fix dhcp bug
    Signed-off-by: nachoparker <nacho@ownyourbits.com>

---
+++ updates/1.16.0.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+## BACKWARD FIXES ( for older images )
+
+source /usr/local/etc/library.sh
+
+# all images
+
+# docker images only
+[[ -f /.docker-image ]] && {
+:
+}
+
+# for non docker images
+[[ ! -f /.docker-image ]] && {
+  # Update btrfs-sync
+  wget -q https://raw.githubusercontent.com/nachoparker/btrfs-sync/master/btrfs-sync -O /usr/local/bin/btrfs-sync
+
+  # work around dhcpcd Raspbian bug
+  # https://lb.raspberrypi.org/forums/viewtopic.php?t=230779
+  # https://github.com/nextcloud/nextcloudpi/issues/938
+  test -f /usr/bin/raspi-config && {
+    apt-get update
+    apt-get install haveged
+    systemctl enable haveged.service
+  }
+}
+
+exit 0
