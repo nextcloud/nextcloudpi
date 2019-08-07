5de80234e14f9eb6de7779825e82ce154d9295cd
paschaef <45995338+paschaef@users.noreply.github.com>
2019-08-07 10:14:32 +0200

    lamp.sh: install additionally needed php-imagick module for nextcloud 16
    Signed-off-by: paschaef <45995338+paschaef@users.noreply.github.com>

--- lamp.sh
+++ lamp.sh
@@ -32,7 +32,7 @@ install()
 
     $APTINSTALL -t $RELEASE php${PHPVER} php${PHPVER}-curl php${PHPVER}-gd php${PHPVER}-fpm php${PHPVER}-cli php${PHPVER}-opcache \
                             php${PHPVER}-mbstring php${PHPVER}-xml php${PHPVER}-zip php${PHPVER}-fileinfo php${PHPVER}-ldap \
-                            php${PHPVER}-intl php${PHPVER}-bz2 php${PHPVER}-json
+                            php${PHPVER}-intl php${PHPVER}-bz2 php${PHPVER}-json php${PHPVER}-imagick
 
     mkdir -p /run/php
 
