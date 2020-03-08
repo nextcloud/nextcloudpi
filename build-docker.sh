4d92209be257736ec6f1ea288f43711f1dc5848c
Tobias K <6317548+thecalcaholic@users.noreply.github.com>
2020-03-08 16:48:40 +0100

    build-docker.sh: Add --push and --help parameters
    Signed-off-by: Tobias K <6317548+thecalcaholic@users.noreply.github.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Batch-build docker container layers for NextCloudPi
 #
 # Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
@@ -9,6 +10,20 @@ set -e
 version=$(git describe --tags --always)
 version=${version%-*-*}
 release=$(jq -r .release < etc/ncp.cfg)
+registry=${2:-ownyourbits}
+# if the 2nd parameter starts with '--' it's not the docker registry
+[[ "$registry" =~ --* ]] && registry="ownyourbits"
+
+function show_help() {
+  echo "USAGE: build-docker.sh arch [registry] [options]"
+  echo ""
+  echo "  arch:     The target architecture for the docker images (one of x86, armhf arm64)"
+  echo "  registry: The docker registry to use for building and publishing (images will be named registry/image-name:tag)"
+  echo ""
+  echo "  OPTIONS:"
+  echo "    --help Show this message"
+  echo "    --push Push images after building"
+}
 
 function docker_build() { DOCKER_BUILDKIT=1 docker build --progress=plain . "$@"; }
 
@@ -19,20 +34,32 @@ function build_arch()
   local arch_qemu="${3}"
   local ncp_tag="${4:-$arch}"
 
-  docker_build -f docker/debian-ncp/Dockerfile  -t ownyourbits/debian-ncp-${ncp_tag}:latest --pull --build-arg release=${release} --build-arg arch=${arch} --build-arg arch_qemu=${arch_qemu}
-  docker_build -f docker/lamp/Dockerfile        -t ownyourbits/lamp-${ncp_tag}:latest              --build-arg release=${release} --build-arg arch=${ncp_tag}
-  docker_build -f docker/nextcloud/Dockerfile   -t ownyourbits/nextcloud-${ncp_tag}:latest         --build-arg release=${release} --build-arg arch=${ncp_tag}
-  docker_build -f docker/nextcloudpi/Dockerfile -t ownyourbits/nextcloudpi-${ncp_tag}:latest       --build-arg release=${release} --build-arg arch=${ncp_tag} --build-arg ncp_ver=${version}
+  docker_build -f docker/debian-ncp/Dockerfile  -t "${registry}/debian-ncp-${ncp_tag}:latest" --pull --build-arg "release=${release}" --build-arg "arch=${arch}" --build-arg "arch_qemu=${arch_qemu}"
+  docker_build -f docker/lamp/Dockerfile        -t "${registry}/lamp-${ncp_tag}:latest"              --build-arg "release=${release}" --build-arg "arch=${ncp_tag}"
+  docker_build -f docker/nextcloud/Dockerfile   -t "${registry}/nextcloud-${ncp_tag}:latest"         --build-arg "release=${release}" --build-arg "arch=${ncp_tag}"
+  docker_build -f docker/nextcloudpi/Dockerfile -t "${registry}/nextcloudpi-${ncp_tag}:latest"       --build-arg "release=${release}" --build-arg "arch=${ncp_tag}" --build-arg "ncp_ver=${version}"
 
-  docker tag ownyourbits/debian-ncp-${ncp_tag}:latest ownyourbits/debian-ncp-${ncp_tag}:"${version}"
-  docker tag ownyourbits/lamp-${ncp_tag}:latest ownyourbits/lamp-${ncp_tag}:"${version}"
-  docker tag ownyourbits/nextcloud-${ncp_tag}:latest ownyourbits/nextcloud-${ncp_tag}:"${version}"
-  docker tag ownyourbits/nextcloudpi-${ncp_tag}:latest ownyourbits/nextcloudpi-${ncp_tag}:"${version}"
+  docker tag "${registry}/debian-ncp-${ncp_tag}:latest" "${registry}/debian-ncp-${ncp_tag}:${version}"
+  docker tag "${registry}/lamp-${ncp_tag}:latest" "${registry}/lamp-${ncp_tag}:${version}"
+  docker tag "${registry}/nextcloud-${ncp_tag}:latest" "${registry}/nextcloud-${ncp_tag}:${version}"
+  docker tag "${registry}/nextcloudpi-${ncp_tag}:latest" "${registry}/nextcloudpi-${ncp_tag}:${version}"
+
 }
 
-[[ "$@" =~ "x86"   ]] && build_arch "${release}" amd64   x86_64  x86
-[[ "$@" =~ "armhf" ]] && build_arch "${release}" arm32v7 arm     armhf
-[[ "$@" =~ "arm64" ]] && build_arch "${release}" arm64v8 aarch64 arm64
+function push_arch() {
+  local suffix="${1}"
+
+  docker push "${registry}/debian-ncp-${ncp_tag}"
+  docker push "${registry}/lamp-${ncp_tag}"
+  docker push "${registry}/nextcloud-${ncp_tag}"
+  docker push "${registry}/nextcloudpi-${ncp_tag}"
+}
+
+[[ "$@" =~ "--help" ]] && show_help && exit 0
+
+[[ "${1}" == "x86"   ]] && build_arch "${release}" amd64   x86_64  x86   && [[ "$@" =~ "--push" ]] && push_arch "x86"
+[[ "${1}" == "armhf" ]] && build_arch "${release}" arm32v7 arm     armhf && [[ "$@" =~ "--push" ]] && push_arch "armhf"
+[[ "${1}" == "arm64" ]] && build_arch "${release}" arm64v8 aarch64 arm64 && [[ "$@" =~ "--push" ]] && push_arch "arm64"
 
 exit 0
 
