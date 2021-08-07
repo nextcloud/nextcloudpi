cdf38590f5a36885b8ac4e1dd49639033d5e2e07
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-08-07 07:25:37 +0200

    ncp-update-nc: Make sure set-nc-domain is being called during update
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

--- bin/ncp/CONFIG/nc-passwd.sh
+++ bin/ncp/CONFIG/nc-passwd.sh
@@ -29,9 +29,9 @@ configure()
     a2dissite ncp-activation
     a2ensite  ncp nextcloud
     nc_domain="$(ncc config:system:get trusted_domains "${TRUSTED_DOMAINS[nc_domain]}")"
-    bash -c "sleep 1.5 && service apache2 reload \
+    bash -c ". /usr/local/etc/library.sh; sleep 1.5 && service apache2 reload \
     && ncc config:system:set trusted_proxies 10 --value=$(dig nextcloudpi +short) >> /var/log/ncp.log \
-    && . /usr/local/etc/library.sh && set-nc-domain '${nc_domain}' >> /var/log/ncp.log
+    && set-nc-domain '${nc_domain}' >> /var/log/ncp.log
     " &>/dev/null &
   fi
 }
