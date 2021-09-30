nextcloud/nextcloudpi

commit 2be666b1f5f43492929bc00872283f6e7ece2443

nc-https: proto logic was inverted fix
Signed-off-by: nachoparker <nacho@ownyourbits.com>

bin/ncp/CONFIG/nc-httpsonly.sh
@@ -12,7 +12,11 @@
 configure()
 {
   [[ -f /.ncp-image ]] && return 0
-  [[ $ACTIVE == "no" ]] && local opt=Off proto=http || local opt=On proto=https
+  if [[ $ACTIVE == "no" ]]; then
+    local opt=Off proto=https
+    else
+      local opt=On proto=http
+  fi
   sed -i "s|RewriteEngine .*|RewriteEngine $opt|" /etc/apache2/sites-available/000-default.conf
   ncc config:system:set overwriteprotocol --value="${proto}"
   apachectl -k graceful

commit 156d94c542c329a30d2e970cfd373719c0706d75

Fix nc-https protocol inverted This change fixes commit 2be666b which caused nc-https logic to be inverted.
Signed-off-by: DJCrapsody <deejaycrapsody@gmail.com>

bin/ncp/CONFIG/nc-httpsonly.sh
@@ -13,9 +13,9 @@ configure()
 {
   [[ -f /.ncp-image ]] && return 0
   if [[ $ACTIVE == "no" ]]; then
-    local opt=Off proto=https
+    local opt=Off proto=http
     else
-      local opt=On proto=http
+      local opt=On proto=https
   fi
   sed -i "s|RewriteEngine .*|RewriteEngine $opt|" /etc/apache2/sites-available/000-default.conf
   ncc config:system:set overwriteprotocol --value="${proto}"