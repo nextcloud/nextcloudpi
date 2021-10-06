8a6c1c08a21629c048f8ec31bf5707c0875d5705
nachoparker <nacho@ownyourbits.com>
2021-10-06 12:08:55 -0600

    ncp-check-nc-version: dont notify the same version more than once
    Signed-off-by: nachoparker <nacho@ownyourbits.com>

--- bin/ncp/UPDATES/nc-notify-updates.sh
+++ bin/ncp/UPDATES/nc-notify-updates.sh
@@ -50,7 +50,7 @@ cat $LATEST > $NOTIFIED
 EOF
   chmod +x /usr/local/bin/ncp-notify-update
 
-  cat > /usr/local/bin/ncp-notify-unattended-upgrade <<EOF
+  cat > /usr/local/bin/ncp-notify-unattended-upgrade <<'EOF'
 #!/bin/bash
 source /usr/local/etc/library.sh
 
@@ -58,25 +58,25 @@ LOGFILE=/var/log/unattended-upgrades/unattended-upgrades.log
 STAMPFILE=/var/run/.ncp-notify-unattended-upgrades
 VERFILE=/usr/local/etc/ncp-version
 
-test -e "\$LOGFILE" || { echo "\$LOGFILE not found"; exit 1; }
+test -e "$LOGFILE" || { echo "$LOGFILE not found"; exit 1; }
 
 # find lines with package updates
-LINE=\$( grep "INFO Packages that will be upgraded:" "\$LOGFILE" )
+LINE=$( grep "INFO Packages that will be upgraded:" "$LOGFILE" )
 
-[[ "\$LINE" == "" ]] && { echo "no new upgrades"; exit 0; }
+[[ "$LINE" == "" ]] && { echo "no new upgrades"; exit 0; }
 
 # extract package names
-PKGS=\$( sed 's|^.*Packages that will be upgraded: ||' <<< "\$LINE" | tr '\\n' ' ' )
+PKGS=$( sed 's|^.*Packages that will be upgraded: ||' <<< "$LINE" | tr '\n' ' ' )
 
 # mark lines as read
-sed -i 's|INFO Packages that will be upgraded:|INFO Packages that will be upgraded :|' \$LOGFILE
+sed -i 's|INFO Packages that will be upgraded:|INFO Packages that will be upgraded :|' \LOGFILE
 
-echo -e "Packages automatically upgraded: \$PKGS\\n"
+echo -e "Packages automatically upgraded: $PKGS\n"
 
 # notify
 notify_admin \
   "NextCloudPi Unattended Upgrades" \
-  "Packages automatically upgraded \$PKGS"
+  "Packages automatically upgraded $PKGS"
 EOF
   chmod +x /usr/local/bin/ncp-notify-unattended-upgrade
 
---
+++ updates/1.41.0.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+## BACKWARD FIXES ( for older images )
+
+source /usr/local/etc/library.sh # sets NCLATESTVER PHPVER RELEASE
+
+# all images
+
+# we handle this ourselves now
+ncc app:disable updatenotification
+run_app nc-notify-updates
+
+# docker images only
+[[ -f /.docker-image ]] && {
+  :
+}
+
+# for non docker images
+[[ ! -f /.docker-image ]] && {
+
+  # make sure redis is up before running nextclud-domain
+  cat > /usr/lib/systemd/system/nextcloud-domain.service <<'EOF'
+[Unit]
+Description=Register Current IP as Nextcloud trusted domain
+Requires=network.target
+After=mysql.service redis.service
+
+[Service]
+ExecStart=/bin/bash /usr/local/bin/nextcloud-domain.sh
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+exit 0
