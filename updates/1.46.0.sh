e1b4ba76b51c3fcd9cf03aa2b587e7e9148843ec
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-12-06 13:12:42 +0100

    notify_push: fix service not restarting on binary update
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

---
+++ updates/1.46.0.sh
@@ -0,0 +1,43 @@
+
+# docker images only
+[[ -f /.docker-image ]] && {
+  cat <<EOF > /etc/cron.daily
+#!/usr/bin/env bash
+. /usr/local/etc/library.sh
+ncc notify_push:self-test || {
+  killall notify_push
+  sleep 1
+  start_notify_push
+}"
+EOF
+}
+
+# for non docker images
+[[ ! -f /.docker-image ]] && {
+  cat > /etc/systemd/system/refresh_notify_push.service <<EOF
+[Unit]
+Description = Restart notify_push service when the NC app is updated
+
+[Service]
+Type = oneshot
+ExecStart = systemctl restart notify_push.service
+
+[Install]
+WantedBy = multi-user.target
+EOF
+  cat > /etc/systemd/system/refresh_notify_push.path <<EOF
+[Unit]
+Description = Path watcher component for refresh_notify_push.service
+
+[Path]
+PathModified = /var/www/nextcloud/apps/notify_push/
+
+[Install]
+WantedBy = multi-user.target
+EOF
+
+  systemctl daemon-reload
+  systemctl enable refresh_notify_push.{path,service}
+  systemctl enable notify_push.service
+  systemctl restart refresh_notify_push.path
+}
