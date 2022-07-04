0d766935dd2c0c4d8e58286ab497ad8cd8b4a8ec
Victor-ray <victorray91@pm.me>
2022-07-04 17:24:59 +0200

    Update dynamics.js
    Signed-off-by: Victor-ray S. <victorray91@pm.me> (Zendai Owl)

--- js/dynamics.js
+++ js/dynamics.js
@@ -45,7 +45,7 @@ function openTab(e, OS) {
 document.addEventListener('DOMContentLoaded', () => {
 
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(document.getElementsByClassName('navbar-burger'), 0);
 
   // Add a click event on each of them
   $navbarBurgers.forEach( el => {
