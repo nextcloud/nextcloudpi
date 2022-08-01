f31148d4bdeb46e0d6df516e5bb17f24fb6561ac
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2022-08-01 05:02:40 +0200

    system_tests.py: Use systemd-run for testing systemd containers

--- tests/system_tests.py
+++ tests/system_tests.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
         pre_cmd = ['lxc', 'exec', 'ncp', '--']
 
     elif systemd_container_running:
-        pre_cmd = ['machinectl', 'shell', 'root@ncp', '/usr/bin/bash', '-c']
+        pre_cmd = ['systemd-run', '--wait', '--machine=ncp', '/bin/bash', '-c']
 
     # SSH method
     else:
