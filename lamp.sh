8a5e13623e10cf4f495251bd4c15fb7a84279aed
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-08-23 18:05:42 +0200

    lamp.sh: Make sure gpg is installed
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- lamp.sh
+++ lamp.sh
@@ -25,11 +25,13 @@ export DEBIAN_FRONTEND=noninteractive
 install()
 {
     set -x
+    apt-get update
+    $APTINSTALL apt-utils cron curl gpg2
     # Setup apt repository for php 8
     wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
     apt-get update
-    $APTINSTALL apt-utils cron curl
+
     ls -l /var/lock || true
     $APTINSTALL apache2
     # Fix missing lock directory
