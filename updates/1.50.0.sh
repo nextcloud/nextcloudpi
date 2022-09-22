b00dff6ae69fc3b74b86183621419279cb362c97
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-09-22 21:52:18 +0200

    1.50.0.sh: stop and start php-fpm service instead of mere restart
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.50.0.sh
+++ updates/1.50.0.sh
@@ -5,5 +5,6 @@ export NCPCFG=/usr/local/etc/ncp.cfg
 source /usr/local/etc/library.sh
 
 
+service "php${PHPVER}-fpm" stop
 clear_opcache
-service "php${PHPVER}-fpm" restart
+service "php${PHPVER}-fpm" start
