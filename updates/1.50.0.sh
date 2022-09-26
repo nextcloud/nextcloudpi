48dbc15565e43ed39a07e1db977c865ed49f3f67
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-09-26 14:28:50 +0200

    1.50.0.sh: Fix update crashing when run from web ui
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.50.0.sh
+++ updates/1.50.0.sh
@@ -5,6 +5,5 @@ export NCPCFG=/usr/local/etc/ncp.cfg
 source /usr/local/etc/library.sh
 
 
-service "php${PHPVER}-fpm" stop
 clear_opcache
-service "php${PHPVER}-fpm" start
+(sleep 5 && service "php${PHPVER}-fpm" restart) &
