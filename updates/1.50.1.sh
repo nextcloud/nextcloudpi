b990f91bb68f5144bee992a80ca6bfff1d8be3e4
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-09-27 11:40:15 +0200

    1.50.1.sh: clear opcache once more, as that process was broken for some users in v1.50.0
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.50.1.sh
+++ updates/1.50.1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -e
+export NCPCFG=/usr/local/etc/ncp.cfg
 source /usr/local/etc/library.sh
 
 install_template systemd/notify_push.service.sh /etc/systemd/system/notify_push.service
+
+
+clear_opcache
+bash -c "sleep 6; service php${PHPVER}-fpm restart" &>/dev/null &
