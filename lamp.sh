679a18fe5be6064012758686dcef8db205ff30cf
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-12-17 00:39:06 +0100

    lamp.sh: Remove unversioned php package
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- lamp.sh
+++ lamp.sh
@@ -47,7 +47,7 @@ install()
     install_with_shadow_workaround --no-install-recommends systemd
     $APTINSTALL -t $RELEASE php${PHPVER} php${PHPVER}-curl php${PHPVER}-gd php${PHPVER}-fpm php${PHPVER}-cli php${PHPVER}-opcache \
                             php${PHPVER}-mbstring php${PHPVER}-xml php${PHPVER}-zip php${PHPVER}-fileinfo php${PHPVER}-ldap \
-                            php${PHPVER}-intl php${PHPVER}-bz2 php-json
+                            php${PHPVER}-intl php${PHPVER}-bz2 # TODO: Add php-json back?
 
     mkdir -p /run/php
 
