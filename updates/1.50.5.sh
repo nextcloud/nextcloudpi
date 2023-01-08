c88b1f274cba059f343d4ae0b165a2746dbe9b9a
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2023-01-08 18:03:14 +0100

    updates/1.50.5.sh: Stop early if nc-snapshot-auto is not available
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.50.5.sh
+++ updates/1.50.5.sh
@@ -5,4 +5,5 @@ export NCPCFG=/usr/local/etc/ncp.cfg
 source /usr/local/etc/library.sh
 
 # Reinstall auto snapshot script to apply fix
+[[ -f "$BINDIR/BACKUPS/nc-snapshot-auto.sh" ]] || exit 0
 run_app nc-snapshot-auto
