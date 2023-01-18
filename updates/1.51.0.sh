65f89e0a0fcbb88fd5acd46ec85d409ab3086afa
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2023-01-18 02:47:57 +0100

    library.sh: Have notify_admin notify *all* admins, not just the first
    1.51.0.sh: clear opcache after removing php-json, notify users if php8.2 packages need to be removed
    
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.51.0.sh
+++ updates/1.51.0.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-apt-get remove -y php-json || true
+set -e
+
+source /usr/local/etc/library.sh
+
+dpkg -l | grep -e '^ii' | grep -e 'php-json' > /dev/null && {
+  apt-get remove -y php-json
+  run_app "clear-php-opcache"
+}
+
+dpkg -l | grep -e '^ii' | grep -e 'php8.2' > /dev/null && {
+  msg="PHP 8.2 packages have been detected on your ncp instance, which could cause issues. If you didn't install them on purpose, please remove them with the following command: sudo apt remove php8.2-*"
+  echo -e "$msg"
+  notify_admin "NextcloudPi" "$msg"
+}
+
+exit 0
