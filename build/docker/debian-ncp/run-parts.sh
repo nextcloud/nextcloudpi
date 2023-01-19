e3fe910517a4ee4c613738103ca98c4ba35aa4c0
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2023-01-19 13:53:06 +0100

    run-parts.sh: Set apache2 env variables
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- build/docker/debian-ncp/run-parts.sh
+++ build/docker/debian-ncp/run-parts.sh
@@ -51,6 +51,15 @@ test -f $FILE || {
 EOF
 chmod +x /usr/local/sbin/update-rc.d
 
+# Set apache2 env variables
+export APACHE_RUN_USER=www-data
+export APACHE_RUN_GROUP=www-data
+export APACHE_LOG_DIR=/var/log/apache2
+export APACHE_LOCK_DIR=/var/lock/apache2
+export APACHE_PID_FILE=/var/run/apache2.pid
+export APACHE_RUN_DIR=/var/run/apache2
+
+
 # Iterate only over 000* entries which might setup environment
 for file in $( ls -1v /etc/services-enabled.d | grep ^000.* ); do
   /etc/services-enabled.d/"$file" start "$1"
