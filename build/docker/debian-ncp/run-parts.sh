9f2c7f6963254892c06141c18d5981826c2ee195
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2023-01-19 14:00:24 +0100

    010lamp: Set apache2 variables in Dockerfile instead
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- build/docker/debian-ncp/run-parts.sh
+++ build/docker/debian-ncp/run-parts.sh
@@ -51,13 +51,6 @@ test -f $FILE || {
 EOF
 chmod +x /usr/local/sbin/update-rc.d
 
-# Set apache2 env variables
-export APACHE_RUN_USER=www-data
-export APACHE_RUN_GROUP=www-data
-export APACHE_LOG_DIR=/var/log/apache2
-export APACHE_LOCK_DIR=/var/lock/apache2
-export APACHE_PID_FILE=/var/run/apache2.pid
-export APACHE_RUN_DIR=/var/run/apache2
 
 
 # Iterate only over 000* entries which might setup environment
