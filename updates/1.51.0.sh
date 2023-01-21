33af0ed554e57e81b225e18bcd7453c9f2abc1f2
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2023-01-21 20:09:24 +0100

    1.51.0.sh: Make sure, Changes to opcache.ini are in effect
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- updates/1.51.0.sh
+++ updates/1.51.0.sh
@@ -10,12 +10,13 @@ dpkg -l | grep -e '^ii' | grep -e 'php-json' > /dev/null && {
 
 echo "Updating opcache configuration..."
 install_template "php/opcache.ini.sh" "/etc/php/${PHPVER}/mods-available/opcache.ini"
-run_app "clear-php-opcache"
 
 dpkg -l | grep -e '^ii' | grep -e 'php8.2' > /dev/null && {
   msg="PHP 8.2 packages have been detected on your ncp instance, which could cause issues. If you didn't install them on purpose, please remove them with the following command: sudo apt remove php8.2-*"
   echo -e "$msg"
   notify_admin "NextcloudPi" "$msg"
 }
+
+bash -c "sleep 5; source /usr/local/etc/library.sh; clear_opcache; service php${PHPVER}-fpm restart;" &> /dev/null &
 
 exit 0
