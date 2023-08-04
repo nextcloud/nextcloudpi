12e385ad1d38e8263ea91f9988c7aceda5308225
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2023-08-05 01:10:34 +0200

    redis.conf.sh: Don't set variables with --defaults parameter
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

--- etc/ncp-templates/redis.conf.sh
+++ etc/ncp-templates/redis.conf.sh
@@ -1,13 +1,12 @@
-source /usr/local/etc/svc/nextcloud/.env
-
 REDIS_MEM=3gb
-[[ -n "$REDIS_PASSWORD" ]] || REDIS_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
 
 if [[ "$1" == "--defaults" ]]
 then
   echo "INFO: Restoring template to default settings" >&2
   REDIS_PASSWORD="default"
 else
+  [ -f /usr/local/etc/svc/nextcloud/.env ] && source /usr/local/etc/svc/nextcloud/.env
+  [[ -n "$REDIS_PASSWORD" ]] || REDIS_PASSWORD="$(base64 < /dev/urandom | head -c 32)"
   REDIS_MEM="$(source "${BINDIR}/CONFIG/nc-limits.sh"; tmpl_redis_mem)"
 fi
 
