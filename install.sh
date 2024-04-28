9fb334cc25f61a4f69c22ed54a25814a0b40f19c
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2024-04-28 22:52:54 +0200

    install.sh,ncp.sh,1.54.0.sh: Disable root login via ssh
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -28,6 +28,17 @@ export PATH="/usr/local/sbin:/usr/sbin:/sbin:${PATH}"
 type mysqld &>/dev/null && echo ">>> WARNING: existing mysqld configuration will be changed <<<"
 type mysqld &>/dev/null && mysql -e 'use nextcloud' &>/dev/null && { echo "The 'nextcloud' database already exists. Aborting"; exit 1; }
 
+[[ "$DEBIAN_FRONTEND" == "noninteractive" ]] || {
+  echo "WARNING: This installer will disable SSH login for the root user and reset its password.
+If you need to login with root, you should make sure, you have a root session open that you can use,
+to revert these changes afterwards (set PermitRootLogin to 'yes' in /etc/ssh/sshd_config and run passwd as root)."
+  for i in {1..10}
+  do
+    echo "Continuing in $((30-(3*i)))s (press Ctrl+C to abort)..."
+    sleep 3
+  done
+}
+
 # get dependencies
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git ca-certificates sudo lsb-release wget jq
