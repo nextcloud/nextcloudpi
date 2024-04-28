19eccd56dce815b87444038eb7dff66560620e54
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2024-04-28 16:48:00 +0200

    ncp.sh: Add link to activation guide
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- ncp.sh
+++ ncp.sh
@@ -246,7 +246,7 @@ EOF
 EOF
 
     echo '
-NCP is not activated yet. Please enter https://nextcloudpi.local or this instance'"'"'s local IP address in your webbrowser to complete activation.
+NCP is not activated yet. Please enter https://nextcloudpi.local or this instance'"'"'s local IP address in your webbrowser to complete activation. You can find detailed instructions at https://nextcloudpi.com/activate
 ' >> /etc/issue
     chmod a+x /etc/update-motd.d/*
 
