22e12e56f5f9add794ca2db6406f9afb1a239d71
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2024-05-18 22:08:54 +0200

    kopia-restore: Reuse repository setup functionality from kopia.sh
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- bin/ncp/BACKUPS/kopia.sh
+++ bin/ncp/BACKUPS/kopia.sh
@@ -32,7 +32,7 @@ get_repository_password() {
 }
 
 get_repository_type() {
-  local repo="${1?}"
+  local repo="${1}"
   [[ -z "${repo}" ]] && {
     [[ -f /usr/local/etc/kopia/repository.config ]] && {
       repo_type="$(jq -er '.storage.type' /usr/local/etc/kopia/repository.config)"
@@ -61,6 +61,106 @@ get_repository_type() {
 
 is_active() {
   [[ -f /usr/local/etc/kopia/repository.config ]] && [[ -f /etc/cron.hourly/ncp-kopia ]]
+}
+
+setup_repository() {
+
+  set -e
+
+  local repo_definition="${1}"
+  local storage_key="${2}"
+  local repository_password="${3}"
+  local hostname="${4?}"
+
+  mkdir -p /usr/local/etc/kopia
+  mkdir -p /var/log/kopia
+
+  kopia_flags=(--config-file=/usr/local/etc/kopia/repository.config --log-dir="/var/log/kopia" --no-persist-credentials)
+  kopia_repo_args=()
+
+  repo_type="$(get_repository_type "${repo_definition}")"
+  repo="${repo_definition#*://}"
+  storage_key="$(echo -n "${storage_key}" | base64 -d)"
+
+  [[ -z "$REPOSITORY" ]] && ! [[ -f /usr/local/etc/kopia/repository.config ]] && {
+    echo "REPOSITORY not set and no repository config found!"
+    return 1
+  }
+
+  if [[ -z "${repo_defintion}" ]]
+  then
+    repo_type=from-config
+    kopia_repo_args=(--file="/usr/local/etc/kopia/repository.config")
+  else
+    if [[ "${repo_type}" == "filesystem" ]]; then
+      repo_path="$(tmpl_repository_local_path)"
+      kopia_repo_args=(--path "${repo_path}")
+    elif [[ "${repo_type}" == "sftp" ]]; then
+      sftp_user="${repo%@*}"
+      sftp_host="${repo#*@}"
+      sftp_host="${sftp_host%:*}"
+      repo_path="${repo#*:}"
+      if [[ -n "${storage_key}" ]]
+      then
+        echo "Installing STORAGE_KEY as ssh private key..."
+        mkdir -p ~root/.ssh
+        touch ~root/.ssh/kopia_key
+        chmod 0600 ~root/.ssh/kopia_key
+        echo "${storage_key}" > ~root/.ssh/kopia_key
+        eval "$(ssh-agent)"
+        ssh-add ~root/.ssh/kopia_key
+        kopia_repo_args+=("--key-data=$(cat ~root/.ssh/kopia_key)")
+        echo "Done."
+      fi
+      ssh -o "BatchMode=yes" -o "StrictHostKeyChecking=no" "${sftp_user}@${sftp_host}" || { echo "SSH non-interactive not properly configured"; return 1; }
+      rm ~root/.ssh/kopia_key
+      kopia_repo_args+=(--host="${sftp_host}" --username="${sftp_user}" --path="${repo_path}" --known-hosts-data="$(cat ~root/.ssh/known_hosts)")
+    elif [[ "${repo_type}" == "b2" ]]; then
+      if [[ -z "$storage_key" ]]; then
+        echo "Key is required for b2 backend, but was not provided"
+        return 1
+      fi
+      key_id="${storage_key%:*}"
+      key_value="${storage_key#*:}"
+      kopia_repo_args+=(--bucket="${repo}" --key-id="${key_id}" --key="${key_value}")
+    elif [[ "${repo_type}" == "s3" ]]; then
+        if [[ -z "$storage_key" ]]; then
+          echo "Key is required for s3 backend, but was not provided"
+          return 1
+        fi
+        key_id="${storage_key%:*}"
+        key_value="${storage_key#*:}"
+        kopia_repo_args+=(--bucket="${repo}" --access-key="${key_id}" --secret-access-key="${key_value}")
+    else
+      echo "Invalid repository type '${repo_type}'"
+      return 1
+    fi
+  fi
+
+  export KOPIA_PASSWORD="$(get_repository_password "${repository_password}")"
+
+  echo "Attempting to connect to existing repository first..."
+  kopia "${kopia_flags[@]}" \
+    repository connect "${repo_type}" \
+      "${kopia_repo_args[@]}" \
+      --override-username nextcloudpi \
+      --override-hostname "$hostname" || {
+    echo "Creating new repository..."
+    kopia "${kopia_flags[@]}" \
+      repository create "${repo_type}" \
+        "${kopia_repo_args[@]}" \
+        --override-username ncp \
+        --override-hostname "$hostname"
+  }
+
+  kopia "${kopia_flags[@]}" cache set --cache-directory="${DESTINATION?DESTINATION must not be empty}/.kopia_cache"
+
+  chmod 0600 /usr/local/etc/kopia/repository.config
+  touch /usr/local/etc/kopia/password
+  chmod 0600 /usr/local/etc/kopia/password
+  chown root: /usr/local/etc/kopia/password
+  echo "${KOPIA_PASSWORD}" > /usr/local/etc/kopia/password
+  echo "Repository initialized successfully"
 }
 
 install() {
@@ -146,76 +246,11 @@ configure() {
 
   set -e
 
-  mkdir -p /usr/local/etc/kopia
-  mkdir -p /var/log/kopia
   hostname="$(ncc config:system:get overwrite.cli.url)"
   hostname="${hostname##http*:\/\/}}"
   hostname="${hostname%%/*}"
 
-  repo_type="$(get_repository_type "${REPOSITORY}")"
-  repo="${REPOSITORY#*://}"
-  STORAGE_KEY="$(echo -n "${STORAGE_KEY}" | base64 -d)"
-
-  kopia_flags=(--config-file=/usr/local/etc/kopia/repository.config --log-dir="/var/log/kopia" --no-persist-credentials)
-  kopia "${kopia_flags[@]}" cache set --cache-directory="${DESTINATION?DESTINATION must not be empty}/.kopia_cache"
-  kopia_repo_args=()
-  if [[ "${repo_type}" == "filesystem" ]]; then
-    repo_path="${REPOSITORY}/ncp-kopia"
-    kopia_repo_args=(--path "${repo_path}")
-  elif [[ "${repo_type}" == "sftp" ]];then
-    sftp_user="${repo%@*}"
-    sftp_host="${repo#*@}"
-    sftp_host="${sftp_host%:*}"
-    repo_path="${repo#*:}"
-    if [[ -n "${STORAGE_KEY}" ]]
-    then
-      echo "Installing STORAGE_KEY as ssh private key..."
-      mkdir -p ~root/.ssh
-      touch ~root/.ssh/kopia_key
-      chmod 0600 ~root/.ssh/kopia_key
-      echo "${STORAGE_KEY}" > ~root/.ssh/kopia_key
-      eval "$(ssh-agent)"
-      ssh-add ~root/.ssh/kopia_key
-      kopia_args+=("--key-data=$(cat ~root/.ssh/kopia_key)")
-      echo "Done."
-    fi
-    ssh -o "BatchMode=yes" -o "StrictHostKeyChecking=no" "${sftp_user}@${sftp_host}" || { echo "SSH non-interactive not properly configured"; return 1; }
-    kopia_repo_args=(--host="${sftp_host}" --username="${sftp_user}" --path="${repo_path}" --known-hosts-data="$(cat ~root/.ssh/known_hosts)")
-  elif [[ "${repo_type}" == "b2" ]]; then
-    if [[ -z "$STORAGE_KEY" ]]; then
-      echo "Key is required for b2 backend, but was not provided"
-      return 1
-    fi
-    key_id="${STORAGE_KEY%:*}"
-    key_value="${STORAGE_KEY#*:}"
-    kopia_repo_args+=(--bucket="${repo}" --key-id="${key_id}" --key="${key_value}")
-  elif [[ "${repo_type}" == "s3" ]]; then
-        echo "Key is required for s3 backend, but was not provided"
-        return 1
-      fi
-      key_id="${STORAGE_KEY%:*}"
-      key_value="${STORAGE_KEY#*:}"
-      kopia_repo_args+=(--bucket="${repo}" --access-key="${key_id}" --secret-access-key="${key_value}")
-  else
-    echo "Invalid (unsupported) repository type '${repo_type}'"
-    return 1
-  fi
-
-  export KOPIA_PASSWORD="$(get_repository_password "${REPOSITORY_PASSWORD}")"
-
-  echo "Attempting to connect to existing repository first..."
-  kopia "${kopia_flags[@]}" \
-    repository connect "${repo_type}" \
-      "${kopia_repo_args[@]}" \
-      --override-username nextcloudpi \
-      --override-hostname "$hostname" || {
-    echo "Creating new repository..."
-    kopia "${kopia_flags[@]}" \
-      repository create "${repo_type}" \
-        "${kopia_repo_args[@]}" \
-        --override-username ncp \
-        --override-hostname "$hostname"
-  }
+  setup_repository "${REPOSITORY}" "${STORAGE_KEY}" "${REPOSITORY_PASSWORD}" "${hostname}"
 
   echo "Configuring backup policy..."
   kopia "${kopia_flags[@]}" \
@@ -230,18 +265,13 @@ configure() {
       --add-ignore '/ncdata/*/uploads' \
       --add-ignore '/ncdata/.data_*'
 
-  touch /usr/local/etc/kopia/password
-  chmod 0600 /usr/local/etc/kopia/password
-  chown root: /usr/local/etc/kopia/password
-  echo "${REPOSITORY_PASSWORD}" > /usr/local/etc/kopia/password
-  echo "Repository initialized successfully"
 
   if [[ "${AUTOMATIC_BACKUPS}" == "yes" ]]
   then
     echo "Enabling automatic backups"
     cat > /etc/cron.hourly/ncp-kopia <<EOF
 #!/bin/bash
-KOPIA_PASSWORD="$(cat /usr/local/etc/kopia/password)" /usr/local/bin/kopia-backup
+/usr/local/bin/kopia-backup
 EOF
     chmod +x /etc/cron.hourly/ncp-kopia
   else
