8fa19a56213c9c1acbaf648fc1f9b0948f84a4e8
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2024-08-19 04:22:37 +0200

    staged_rollouts/v1.54.3.txt: Expand rollout to 60%
    Signed-off-by: Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>

--- generate_cohortes.sh
+++ generate_cohortes.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
-
 #min=$((101-${1:-50}))
+total_count="${1?}"
+target_file="${2?}"
 all=({100..1})
 cohortes=()
-for i in "${all[@]:0:${1?}}"
+
+while IFS="" read -r line || [ -n "$line" ]
+do
+  [[ -n "$line" ]] || continue
+  cohortes+=("$line")
+done < "${target_file}"
+
+cohortes_count=${#cohortes[@]}
+count=$(( total_count - cohortes_count ))
+
+for i in "${all[@]:0:${count}}"
 do
   cohorte_id=$((RANDOM % i))
   while [[ " ${cohortes[*]} " =~ .*" ${cohorte_id} ".* ]]
   do
     cohorte_id=$((cohorte_id+1))
+    if [[ $cohorte_id -eq 101 ]]
+    then
+      cohorte_id=1
+    fi
   done
   cohortes+=($cohorte_id)
 done
 
-echo "${cohortes[*]}" | tr ' ' $'\n'
+(IFS=$'\n'; echo -n "${cohortes[*]}" | sort -h | head -c -1 >| "$target_file")
