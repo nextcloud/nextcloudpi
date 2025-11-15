5b11ae0caf7131b6e8dd0b3be5f8f7622a17df16
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2025-11-15 03:38:49 +0100

    nc-nextcloud.sh, ncp-provisioning.sh: Implement nextcloud service workers

--- updates/1.56.0.sh
+++ updates/1.56.0.sh
@@ -5,4 +5,27 @@ then
   ncc app:disable app_api
 fi
 
+cat > /etc/systemd/system/nextcloud-ai-worker@.service <<'EOF'
+[Unit]
+Description=Nextcloud AI worker %i
+After=network.target
+
+[Service]
+ExecStart=php occ background-job:worker -t 60 'OC\\TaskProcessing\\SynchronousBackgroundJob'
+Restart=always
+StartLimitInterval=60
+StartLimitBurst=10
+WorkingDirectory=/var/www/nextcloud
+User=www-data
+
+[Install]
+WantedBy=multi-user.target
+EOF
+max="$(nproc || echo '2')"
+max="$((max-1))"
+for i in $(seq 1 "$max")
+do
+  systemctl enable --now "nextcloud-ai-worker@${i}.service"
+done
+
 exit 0
