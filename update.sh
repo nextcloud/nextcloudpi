ace1b97f3848576e18ea13a31acef96eadbd2d8a
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2026-04-03 02:15:06 +0200

    Revert "update.sh: Update templates during updates"
    This reverts commit dcc22ce9fd388f1e668b742a6f7a24fe4f3785f2.

--- update.sh
+++ update.sh
@@ -126,8 +126,6 @@ for file in etc/ncp-config.d/*; do
 
 done
 
-cp -r etc/ncp-templates /usr/local/etc/
-
 # update NCVER in ncp.cfg and nc-nextcloud.cfg (for nc-autoupdate-nc and nc-update-nextcloud)
 nc_version=$(jq -r .nextcloud_version < etc/ncp.cfg)
 cfg="$(jq '.' /usr/local/etc/ncp.cfg)"
